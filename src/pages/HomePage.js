e022d969cd2288a4d24ffbb93886d1a387f17e5e
Lisandro <salvareschilisandro0@gmail.com>
2021-06-10 18:21:24 -0300

    finish homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,18 @@
-import React from "react";
+import NavBar from './NavBar';
+import Slider from './Slider';
+import Testimonios from './Testimonios';
+import Footer from './Footer';
+import Data from "../assets/fakedata"
 
 function HomePage() {
-  return <div>Esta es la HomePage</div>;
+    return (
+        <>
+            <NavBar />
+            <Slider />
+            <Testimonios people={Data} />
+            <Footer />
+        </>
+    )
 }
 
 export default HomePage;
