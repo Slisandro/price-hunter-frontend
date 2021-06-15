3c29686f927c47790fcd356bda294c14737eb515
nicoarguello <nicoarguello36@gmail.com>
2021-06-15 12:10:13 -0300

    agregue ruta pos productos admin

--- src/components/Admin/NavbarAdmin/NavbarAdm.js
+++ src/components/Admin/NavbarAdmin/NavbarAdm.js
@@ -4,7 +4,27 @@ import user from "../../../assets/user (1).png";
 import { Link } from "react-router-dom";
 // import notificacion from "../../assets/";
 
+//----------------------------------------
+//         PRUEBA PARA POST ADMIN
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {unidadDeMedida, pais, tipoUsuario, genero, monedas, ciudad, region, familia, categoria, subcategoria, tipoTransaccion, transaccion, clientes, desafio, productos } from '../../../components/Redux/actions'
+//----------------------------------------
+ 
+
+
 const NavbarAdm = (props) => {
+  //----------------------------------------
+  //         PRUEBA PARA POST ADMIN
+
+  const m= useSelector(x => x.obj)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(productos(m));
+  }, [dispatch, m]);
+
+  //-----------------------------------------
   return (
     <nav className="navbar">
       <div className="nav_icon" onClick={() => props.openSidebar()}>
