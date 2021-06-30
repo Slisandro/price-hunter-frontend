afb46139df788e66627a1adc1684b7820a6beb14
cristian1098 <cfernandezpando@gmail.com>
2021-06-29 22:54:56 -0300

    tipoUsuario,um,tipoTransaccion

--- src/components/Admin/FormPuts/PutTipoTrans/PutTipoTrans.js
+++ src/components/Admin/FormPuts/PutTipoTrans/PutTipoTrans.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { putTipoTransaccion, getTipoTransaccion } from "../../../Redux/actions";
 import { useForm } from "react-hook-form";
@@ -26,6 +26,29 @@ function PutTipoTrans() {
   useEffect(() => {
     dispatch(getTipoTransaccion());
   }, [dispatch]);
+
+
+  const [state, setState] = useState({
+    tipo_transaccion: "",
+    id: "",
+  });
+  const ChangeInput = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    if (name === "tipo_transaccion") {
+      setState({
+        ...state,
+        [name]: value,
+      });
+    }
+    if (name === "id") {
+      setState({
+        ...state,
+        [name]: value,
+      });
+    }
+  };
 
   const {
     register,
@@ -62,28 +85,32 @@ function PutTipoTrans() {
       {/* <Col lg="4"> */}
       <Card className="card-chart">
         <CardHeader>
-          <h1 id="title">Tipo de Transacción</h1>
+          <span id="title">Tipo de Transacción</span>
         </CardHeader>
         <CardBody>
           <Form
             id="survey-form"
             className="form"
             noValidate
-            // onChange={(e) => ChangeInput(e)}
+           onChange={(e) => ChangeInput(e)}
             onSubmit={handleSubmit(submit)}
           >
             <Row>
               <Col>
-                <label className="title">Tipos de Transacción</label>
+                <h6 className="title">Tipos de Transacción</h6>
+                {state.id.length === 0 ? (
+                  <>
+
                 <Input
                   name="id"
                   type="select"
                   className="inp"
+                  onChange={(e) => ChangeInput(e)}
                   {...register("id", {
-                    //   required: {
-                    //     value: true,
-                    //     message: "Debe seleccionar un tipo de transaccion",
-                    //   },
+                       required: {
+                         value: true,
+                         message: "Debe seleccionar un tipo de transaccion",
+                       },
                   })}
                 >
                   <option></option>
@@ -94,20 +121,52 @@ function PutTipoTrans() {
                   ))}
                 </Input>
                 <span className="err">{errors?.id?.message}</span>
-
-                <div>
-                  <label className="title">Nuevo Tipo de Transacción</label>
+              
+                   </>
+                ) : (
+                  <>
                   <Input
+                  valid
+                  name="id"
+                  type="select"
+                  className="inp"
+                  onChange={(e) => ChangeInput(e)}
+                  {...register("id", {
+                       required: {
+                         value: true,
+                         message: "Debe seleccionar un tipo de transaccion",
+                       },
+                  })}
+                >
+                  <option></option>
+                  {transaccion.map((f, index) => (
+                    <option key={index} value={f.id}>
+                      {f.tipo_transaccion}
+                    </option>
+                  ))}
+                </Input>
+                <span className="err">{errors?.id?.message}</span>
+  
+                   </>
+                )
+                }
+                
+                <div style={{marginTop: "1rem" }}>
+                  <h6 className="title">Nuevo Tipo de Transacción</h6>
+                  {!state.tipo_transaccion ? (
+                    <>
+                    
+                    <Input
                     // className="inp"
                     type="text"
                     name="tipo_transaccion"
                     autoComplete="off"
                     max="0"
                     {...register("tipo_transaccion", {
-                      // required: {
-                      //   value: true,
-                      //   message: "Debe ingresar una tipo transaccion ",
-                      // },
+                       required: {
+                        value: true,
+                         message: "Debe ingresar una tipo transaccion ",
+                       },
                       maxLength: {
                         value: 15,
                         message:
@@ -122,6 +181,72 @@ function PutTipoTrans() {
                   <span className="err">
                     {errors?.tipo_transaccion?.message}
                   </span>
+                  
+                     </>
+                  ): state.tipo_transaccion.length && state.tipo_transaccion.length < 15 ? (
+                    <>
+
+                    <Input
+                    valid
+                    type="text"
+                    name="tipo_transaccion"
+                    autoComplete="off"
+                    max="0"
+                    {...register("tipo_transaccion", {
+                      required: {
+                      value: true,
+                      message: "Debe ingresar una tipo transaccion ",
+                       },
+                      maxLength: {
+                        value: 15,
+                        message:
+                          "El nombre no debe tener mas de quince letras!",
+                      },
+                      max: {
+                        value: 0,
+                        message: "El nombre no puede comenzar con numeros",
+                      },
+                    })}
+                  />
+                  <span className="err">
+                    {errors?.tipo_transaccion?.message}
+                  </span>
+                  
+                     </>
+                  ) : (
+
+                    <>
+                    <Input
+                    invalid
+                    type="text"
+                    name="tipo_transaccion"
+                    autoComplete="off"
+                    max="0"
+                    {...register("tipo_transaccion", {
+                       required: {
+                         value: true,
+                         message: "Debe ingresar una tipo transaccion ",
+                       },
+                      maxLength: {
+                        value: 15,
+                        message:
+                          "El nombre no debe tener mas de quince letras!",
+                      },
+                      max: {
+                        value: 0,
+                        message: "El nombre no puede comenzar con numeros",
+                      },
+                    })}
+                  />
+                  <span className="err">
+                    {errors?.tipo_transaccion?.message}
+                  </span>
+                  
+                     </>
+                  )
+                   
+                  }
+                
                 </div>
                 <Button
                   className="btn-fill"
