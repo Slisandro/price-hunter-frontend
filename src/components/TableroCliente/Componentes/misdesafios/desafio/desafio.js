06d99923bb8fa1a61e37179f0e069309820c35a5
marco1367 <marcopablodemaio@gmail.com>
2021-07-02 02:30:33 -0300

    restriccion de editar desafios finalizados

--- src/components/TableroCliente/Componentes/misdesafios/desafio/desafio.js
+++ src/components/TableroCliente/Componentes/misdesafios/desafio/desafio.js
@@ -14,6 +14,41 @@ function Desafio({desafio}) {
   const [stateModal, setStateModal] = useState({ abierto: false });
   const [stateModal2, setStateModal2] = useState({ abierto: false });
 
+
+  //-----funcion para limitar las fechas min en el form-------//
+//----------------------------------------------------------//
+function chequeofecha(val) {
+  let str = val.toString()
+  if (str.length < 2) {
+    return str = "0" + str;
+  }
+  return str
+}
+
+const año_min = new Date().getFullYear();
+const mes_min = new Date().getMonth() + 1;
+const dia_min = new Date().getDate();
+const fecha_min = año_min.toString() + "-" + chequeofecha(mes_min) + "-" + chequeofecha(dia_min);
+const fecha_min_slice = fecha_min.slice(2,10)
+
+
+const año_des_min = (parseInt(fecha_min_slice.slice(0,2))+2000)*1000;
+const mes_des_min = parseInt( fecha_min_slice.slice(3,5) )*100;
+const dia_des_min = parseInt( fecha_min_slice.slice(6,8) );
+const fecha_min_acumulada = año_des_min + mes_des_min + dia_des_min;
+// console.log(fecha_min_acumulada)
+//----------------------------------------------------------//
+//----------------------------------------------------------//
+ 
+//--------------------fecha final acumulada--------------//
+const año_des = (parseInt(desafio.fecha_final.slice(0,2))+2000)*1000;
+const mes_des = parseInt( desafio.fecha_final.slice(3,5) )*100;
+const dia_des = parseInt( desafio.fecha_final.slice(6,8) );
+const fecha_final_desafio_acumulada = año_des + mes_des + dia_des;
+// console.log(fecha_final_desafio_acumulada)
+//---------------------------------------------------------//
+
+
   //-------FUNCIONES CALLBACK PARA MODAL DETALLE------//
   //--------------------------------------------------//
   function abrirModal() {
@@ -36,7 +71,12 @@ function Desafio({desafio}) {
                 {/* <div> <Button style={styles_buttn} ><img className="icono-desafio-listadesafios-cliente" src={ojo} /></Button>  </div> 
                 <div> <Button style={styles_buttn} ><img  className="icono-desafio-listadesafios-cliente" src={lapiz} /></Button>  </div>  */}
                 <div> <button onClick={()=>{abrirModal()}} ><img className="icono-desafio-listadesafios-cliente" src={ojo} /></button>  </div> 
+
+                {
+                  (fecha_final_desafio_acumulada < fecha_min_acumulada ) ? null 
+                  :
                 <div> <button onClick={()=>{abrirModal2()}} ><img  className="icono-desafio-listadesafios-cliente" src={lapiz} /></button>  </div> 
+                }
             </div>
 
             <ModalDetalleDesafio desafio={desafio} abierto={stateModal.abierto} abrirModal={abrirModal} />
